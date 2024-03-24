b011773a608a629fb5607738c4a423ecce19bf5c
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-03-24 14:42:39 +0300

    add getSoup

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
-fun main(){
-    println("Hello, Kotlin")
+import dictionary.getSoup
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+fun main() {
+    val url = "https://www.britannica.com/dictionary/"
+    val doc = getSoup(url)
+    println(doc)
 }
--- src/dictionary/britannica.kt
+++ src/dictionary/britannica.kt
@@ -1,2 +1,24 @@
 package dictionary
 
+// Britannica.kt
+
+/**
+ * Module for Britannica Dictionary
+ */
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+/**
+ * Get Jsoup Document object for a URL
+ *
+ * @param url The URL to fetch and parse
+ * @return Jsoup Document object for the URL, or null if unable to retrieve
+ */
+fun getSoup(url: String): Document? {
+    return try {
+        Jsoup.connect(url).get()
+    } catch (e: Exception) {
+        println("An error occurred: $e")
+        null
+    }
+}
